656b551e9fafaf41089e87b448ec4285e765b672
Megan <megangilling1@icloud.com>
2025-04-29 10:09:16 +0100

    Issue with selectArticles not showing up when I pull down. Adding again.

--- app/model.js
+++ app/model.js
@@ -36,13 +36,7 @@ exports.selectArticles = () => {
       LEFT JOIN comments 
         ON articles.article_id = comments.article_id 
       GROUP BY 
-        articles.article_id, 
-        articles.title, 
-        articles.topic, 
-        articles.author, 
-        articles.created_at, 
-        articles.votes, 
-        articles.article_img_url
+        articles.article_id
       ORDER BY articles.created_at DESC
     `)
     .then(({rows}) => {
