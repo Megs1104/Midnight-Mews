c56c55e6012666b3c218482c84949c8c1847c6ef
Megan <megangilling1@icloud.com>
2025-04-30 14:11:57 +0100

    Adding hosting but corrected.

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 const config = {}
 
 if (ENV === "production"){
-    config.conectionString = process.env.DATABASE_URL;
+    config.connectionString = process.env.DATABASE_URL;
     config.max = 2;
 }
 const db = new Pool(config);
--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -1,9 +1,16 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV || "development"}`
+});
+
+const ENV = process.env.NODE_ENV || "development";
 const devData = require('../data/development-data/index.js');
 const seed = require('./seed.js');
 const db = require('../connection.js');
 
+
 const runSeed = () => {
-  return seed(devData).then(() => db.end());
+  return seed(devData)
+  .then(() => db.end());
 };
 
 runSeed();
